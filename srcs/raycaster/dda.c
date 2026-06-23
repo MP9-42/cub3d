1deba57e112af765b03e97db31f30caa387305a7
Alexander Gebert <alegeber@1-B-25.42heilbronn.de>
2026-06-23 13:39:01 +0200

    introduced init_dda and init_step_side function to dda.c

--- srcs/raycaster/dda.c
+++ srcs/raycaster/dda.c
@@ -6,7 +6,7 @@
 /*   By: alegeber <alegeber@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/19 14:29:14 by alegeber          #+#    #+#             */
-/*   Updated: 2026/06/23 11:12:02 by alegeber         ###   ########.fr       */
+/*   Updated: 2026/06/23 11:23:19 by alegeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,42 @@ static int is_wall(t_map *map, int x, int y)
 	return (map->rmap[y][x] == '1');
 }
 
+static void	init_dda(t_player *player, t_ray *ray)
+{
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	if (ray->dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->dir_x);
+	if (ray->dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->dir_y);
+}
+
+static void	init_step_side(t_player *player, t_ray *ray)
+{
+	if (ray->dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x);
+		ray->side_dist_x *= ray->delta_dist_x;
+	}
+	if (ray->dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y);
+		ray->side_dist_y *= ray->delta_dist_y;
+	}
+}
