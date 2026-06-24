61517691ff814b77d3ffb879c0216fbf5ddf1ae5
Alexander Gebert <alegeber@2-D-7.42heilbronn.de>
2026-06-24 14:04:01 +0200

    reintroduce dda.c, add render.c including render_frame, draw_column and wall_color

---
+++ srcs/raycaster/dda.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dda.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegeber <alegeber@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 18:11:15 by alegeber          #+#    #+#             */
+/*   Updated: 2026/06/24 14:03:00 by alegeber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+static int	is_wall(t_map *map, int x, int y)
+{
+	int	len;
+
+	if (y < 0 || y >= map->size || x < 0)
+		return (1);
+	len = (int)ft_strlen(map->rmap[y]);
+	if (x >= len)
+		return (1);
+	return (map->rmap[y][x] == '1');
+}
+
+static void	init_dda(t_player *player, t_ray *ray)
+{
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	if (ray->dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1 / ray->dir_x);
+	if (ray->dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1 / ray->dir_y);
+}
+
+static void	init_step_side(t_player *player, t_ray *ray)
+{
+	if (ray->dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x) * ray->delta_dist_x;
+	}
+	if (ray->dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y) * ray->delta_dist_y;
+	}
+}
+
+static void	perform_dda(t_map *map, t_ray *ray)
+{
+	while (!is_wall(map, ray->map_x, ray->map_y))
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+		}
+	}
+}
+
+void	cast_ray(t_player *player, t_map *map, t_ray *ray, double camera_x)
+{
+	ray->dir_x = player->dir_x + player->plane_x * camera_x;
+	ray->dir_y = player->dir_y + player->plane_y * camera_x;
+	init_dda(player, ray);
+	init_step_side(player, ray);
+	perform_dda(map, ray);
+	if (ray->side == 0)
+		ray->perp_wall_dist = ray->side_dist_x - ray->delta_dist_x;
+	else
+		ray->perp_wall_dist = ray->side_dist_y - ray->delta_dist_y;
+}
---
+++ srcs/raycaster/render.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alegeber <alegeber@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/23 12:45:11 by alegeber          #+#    #+#             */
+/*   Updated: 2026/06/24 14:02:58 by alegeber         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+static uint32_t	wall_color(t_ray *ray)
+{
+	if (ray->side == 0 && ray->step_x > 0)
+		return (0xD9534FFF);
+	if (ray->side == 0)
+		return (0xE8A33DFF);
+	if (ray->step_y > 0)
+		return (0x5CB85CFF);
+	return (0x428BCAFF);
+}
+
+static void	draw_column(t_game *game, t_colors *colors, t_ray *ray, int x)
+{
+	int			y;
+	uint32_t	color;
+
+	if (ray->perp_wall_dist < 0.01)
+		ray->perp_wall_dist = 0.01;
+	ray->line_height = (int)(HEIGHT / ray->perp_wall_dist);
+	ray->draw_start = HEIGHT / 2 - ray->line_height / 2;
+	if (ray->draw_start < 0)
+		ray->draw_start = 0;
+	ray->draw_end = HEIGHT / 2 + ray->line_height / 2;
+	if (ray->draw_end >= HEIGHT)
+		ray->draw_end = HEIGHT - 1;
+	color = wall_color(ray);
+	y = 0;
+	while (y < ray->draw_start)
+		mlx_put_pixel(game->img, x, y++, colors->ceiling);
+	while (y <= ray->draw_end)
+		mlx_put_pixel(game->img, x, y++, color);
+	while (y < HEIGHT)
+		mlx_put_pixel(game->img, x, y++, colors->floor);
+}
+
+void	render_frame(void *param)
+{
+	t_cub	*cub;
+	t_ray	ray;
+	double	camera_x;
+	int		x;
+
+	cub = (t_cub *)param;
+	x = 0;
+	while (x < WIDTH)
+	{
+		camera_x = 2.0 * x / WIDTH - 1.0;
+		cast_ray(cub->player, cub->map, &ray, camera_x);
+		draw_column(cub->game, cub->colors, &ray, x);
+		x++;
+	}
+}
